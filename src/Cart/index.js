29c49baa14061dc6024e36b93eeca6ad69921d5d
karthikcd7 <karthikcd7@gmail.com>
2023-12-06 03:24:40 -0500

    cart is empty

--- src/Cart/index.js
+++ src/Cart/index.js
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import { RemoveFromCart } from "./client";
+import { Link } from "react-router-dom";
 import "./index.css";
 function Cart() {
   const dispatch = useDispatch();
@@ -36,30 +37,37 @@ function Cart() {
   return (
     <Container>
       <h1 className="my-4">Cart</h1>
-      {cart.map((item) => {
-        if (item.user_id == currentUser.id) {
-          const product = products.find((product) => product.id === item.product_id);
-          console.log("product", product);
-          return (
-            <Card key={item.product_id} className="mb-4">
-              <Row className="no-gutters">
-                <Col md={4}>
-                  <Card.Img variant="top" src={product.image} className="cart-product-image" />
-                </Col>
-                <Col md={8}>
-                  <Card.Body>
-                    <Card.Title>{product.title}</Card.Title>
-                    <Card.Text className="product-description">{product.description}</Card.Text>
-                    <Button variant="danger" onClick={() => handleRemoveFromCart(item.id)}>
-                      Remove from Cart
-                    </Button>
-                  </Card.Body>
-                </Col>
-              </Row>
-            </Card>
-          );
-        }
-      })}
+      {cart.length === 0 ? (
+        <div>
+          <p>Your cart is empty.</p>
+          <Link to="/home">Go to Home Page</Link>
+        </div>
+      ) : (
+        cart.map((item) => {
+          if (item.user_id == currentUser.id) {
+            const product = products.find((product) => product.id === item.product_id);
+            console.log("product", product);
+            return (
+              <Card key={item.product_id} className="mb-4">
+                <Row className="no-gutters">
+                  <Col md={4}>
+                    <Card.Img variant="top" src={product.image} className="cart-product-image" />
+                  </Col>
+                  <Col md={8}>
+                    <Card.Body>
+                      <Card.Title>{product.title}</Card.Title>
+                      <Card.Text className="product-description">{product.description}</Card.Text>
+                      <Button variant="danger" onClick={() => handleRemoveFromCart(item.id)}>
+                        Remove from Cart
+                      </Button>
+                    </Card.Body>
+                  </Col>
+                </Row>
+              </Card>
+            );
+          }
+        })
+      )}
     </Container>
   );
 }
