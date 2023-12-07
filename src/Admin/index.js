41d602ce590c00314c4a917f38317f1635970312
Shrada Chellasami <shradac@gmail.com>
2023-12-06 20:42:07 -0500

    admin page bootstrap

--- src/Admin/index.js
+++ src/Admin/index.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { getAllUsers } from '../ProductDetails/Comments/client';
+import "./index.css"
 import { useAuth } from '../Home/AuthContext';
-const AdminPage = () => {
-  const [users, setUsers] = useState(null);
-  const {user} = useAuth();
+import { getAllUsers } from '../ProductDetails/Comments/client';
+const Admin = () => {
+  const [users, setUsers] = useState([]);
+  const { user } = useAuth();
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = getAllUsers().then((users) => setUsers(users));
+       const response = getAllUsers().then((users) => setUsers(users));
       } catch (error) {
         console.log(error);
       }
@@ -19,36 +20,56 @@ const AdminPage = () => {
   }, []);
 
   return (
-    <div>
-    <h1>Admin Page</h1>
-    {user && user.role === 3 && 
-    <div>
-      
-      <h2>Buyers</h2>
-      <ul>
-        {users && users
-          .filter((user) => user.role === 1)
-          .map((user) => (
-            <li key={user.id}>
-              <a href={`/profile/${user.id}`}>{user.username}</a>
-            </li>
-          ))}
-      </ul>
-      <h2>Sellers</h2>
-      <ul>
-        {users && users
-          .filter((user) => user.role === 2)
-          .map((user) => (
-            <li key={user.id}>
-              <a href={`/profile/${user.id}`}>{user.username}</a>
-            </li>
-          ))}
-      </ul>
+    <div className='col-md-10 m-5'>
+      <h1>Admin Page</h1>
+      {users && users.length > 0 && user.role === 3 && (
+        <div>
+          <h2>Buyers</h2>
+          <table className="table table-bordered two-column-table" >
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users
+                .filter((user) => user.role === 1)
+                .map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>
+                      <a href={`/profile/${user.id}`}>{user.username}</a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <h2>Sellers</h2>
+          <table className="table table-bordered two-column-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users
+                .filter((user) => user.role === 2)
+                .map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>
+                      <a href={`/profile/${user.id}`}>{user.username}</a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
-    }
-    </div>
-  
   );
 };
 
-export default AdminPage;
+export default Admin;
